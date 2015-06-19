ad8a2dfb5c49537561dd7e50497ea77d937faf71
Jo <Jo@Ash.local>
2015-06-19 21:58:45 +0200

    jumping logic

--- client/js/player.js
+++ client/js/player.js
@@ -8,7 +8,10 @@ function Player(game,map) {
 	this.speed = 120;
 	this.alive = false;
 	this.jumpButton = null;
-		this.bpmText = null;
+	this.bpmText = null;
+	this.dodgeWindow = false;
+	this.jumpWindow = false;
+	this.bunnyKiller = false;
 };
 
 Player.prototype = {
@@ -19,10 +22,10 @@ Player.prototype = {
 
 		this.game.physics.arcade.enable(this.sprite);	
 
-	  this.sprite.animations.add('left', [0, 1, 2, 3], 10, true);
-    this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
+	  	this.sprite.animations.add('left', [0, 1, 2, 3], 10, true);
+    	this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
 
-		this.game.physics.arcade.gravity.y = 250;
+		this.game.physics.arcade.gravity.y = 500;
 
 		this.sprite.body.collideWorldBounds = true;
 
@@ -41,6 +44,12 @@ Player.prototype = {
 		this.alive = true;
 		this.sprite.x = x;
 		this.sprite.y = y;
+	},
+	dodgeReset: function() {
+		this.dodgeWindow = false;
+	},
+	jumpReset: function() {
+		this.jumpWindow = false;
 	},
 	update: function() {
 			  
@@ -66,9 +75,21 @@ Player.prototype = {
     }
 
     //  Allow the player to jump if they are touching the ground.
-    if (this.jumpButton.isDown  && this.sprite.body.onFloor() ){
-      this.sprite.body.velocity.y = -300;
+    if (this.jumpButton.isDown  && ((this.sprite.body.onFloor() && this.bunnyKiller === false) || this.jumpWindow) ){
+      this.bunnyKiller = true;
+      this.sprite.body.velocity.y = -200;
       this.bmpText.destroy();
+      if (this.sprite.body.onFloor()) {
+      	this.jumpWindow = true;
+		this.game.time.events.add(500,this.jumpReset,this);
+	    }
+    }
+
+    if (!this.jumpButton.isDown){
+    	this.jumpReset();
+    	if(this.sprite.body.onFloor()){
+    		this.bunnyKiller = false;
+    	}
     }
 		
 	
