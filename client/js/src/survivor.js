6966e1e05b650871a2781e03f0e8e5ecdf1495f0
Jannis Reinsch <jannis@brntns.com>
2015-07-11 03:12:19 +0200

    removed hello

--- client/js/src/survivor.js
+++ client/js/src/survivor.js
@@ -13,13 +13,8 @@ Survivor.prototype = {
 		this.sprite = this.game.add.sprite(32, this.game.world.height - 150, 'blackdude');
 	  this.sprite.animations.add('left', [0, 1, 2, 3], 10, true);
     this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
-
 		this.sprite.reset(x, y);
 		this.game.survivors.push(this);
-		this.greeting = this.game.add.sprite( 0, 0, 'hello');
-    this.greeting.visible = false;
-
-
 	},
 	update: function() {
 	//	console.log(this.sprite.status);
@@ -33,19 +28,11 @@ Survivor.prototype = {
 		  this.sprite.animations.stop();
       this.sprite.frame = 4;
 		}
-		if(this.sprite.status === 'hello'){
-		 	this.greeting.visible = true;
-   	 	this.hello(this.sprite.x, this.sprite.y);
-		}
+
 	},
 	jumpReset: function() {
 		 this.greeting.visible = false;
-	},
-	 hello: function(x,y){
-      this.greeting.x = x -32;
-      this.greeting.y = y -60;
-
-  }
+	}
 };
 
 module.exports = Survivor;
