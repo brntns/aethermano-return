8463cb2af44ae06247d124eefd2b45f3fd8465d0
Jo <Jo@Ash.local>
2015-07-24 15:38:20 +0200

    survivor fixes

--- client/js/dist/bundle.js
+++ client/js/dist/bundle.js
@@ -1434,7 +1434,7 @@ var movement = {
         }
       }
       if (this.moveMode === 3) {
-        if (this.jumpButton.isDown || !this.onLadder) {
+        if (this.jumpButton.isDown || !this.onLadder || this.sprite.body.onFloor) {
           this.switchToNormal();
         }
         this.directions();
@@ -1739,7 +1739,7 @@ var movement = {
   climbLadder: function climbLadder() {
     var upspeed = 150;
     var downspeed = 150;
-    var sidespeed = 50;
+    var sidespeed = 75;
     if (!this.mountingLadder) {
       this.sprite.body.velocity.y = 0;
       if (!this.cursors.up.isDown) {
@@ -2425,6 +2425,7 @@ function Survivor(id, game) {
 	this.id = id;
 	this.game = game;
 	this.sprite = null;
+	this.lastStatus = 0;
 };
 
 Survivor.prototype = {
@@ -2433,8 +2434,8 @@ Survivor.prototype = {
 		this.sprite = this.game.survivorGroup.getFirstDead();
 		this.sprite = this.game.add.sprite(32, this.game.world.height - 150, 'explorer');
     // adding animations
-    	this.sprite.animations.add('right', [2,3,4], 10, true);
-    	this.sprite.animations.add('left', [12,13,14], 10, true);
+    this.sprite.animations.add('right', [2,3,4], 10, true);
+    this.sprite.animations.add('left', [12,13,14], 10, true);
 		this.sprite.animations.add('death', [20,21,22,23,24,25,26,27], 10, false);
 		this.sprite.animations.add('monk_slash_rightup', [36,35,37,38,39,36,41,40], 16, true);
 		this.sprite.animations.add('monk_slash_leftup', [46,45,47,48,49,46,31,30], 16, true);
@@ -2455,45 +2456,86 @@ Survivor.prototype = {
 		this.game.survivors.push(this);
 	},
 	update: function() {
-
-		if(this.sprite.status === 0){
-			//this.sprite.animations.stop();
-			this.sprite.frame = 4;
-		}
-		else if(this.sprite.status === 1){
-
-		}
-		else if(this.sprite.status === 2){
-				console.log(this.sprite.status);
-				console.log('left');
-				this.sprite.animations.play('left');
-		}
-		else if(this.sprite.status === 3){
-			this.sprite.animations.play('right');
-		}
-		else if(this.sprite.status === 4){
-		}
-		else if(this.sprite.status === 5){
-
-		}
-		else if(this.sprite.status === 6){
-			this.sprite.animations.play('death');
-		}
-		else if(this.sprite.status === 100){
-		this.sprite.loadTexture('explorer', 0);
-		}
-		else if(this.sprite.status === 101){
-		this.sprite.loadTexture('monk', 0);
-		}
-		else if(this.sprite.status === 102){
-			this.sprite.loadTexture('tron', 0);
-		}
-		else if(this.sprite.status === 103){
-			this.sprite.loadTexture('wizard', 0);
-		}
-
-
-	}
+		switch (this.sprite.status) {
+		case 0: //Idle
+  		if(this.lastStatus !== 0){
+  			//this.sprite.animations.stop();
+  			this.sprite.frame = 4;
+  			this.lastStatus = 0;
+  		}
+		break;
+    case 1: //Waving
+      if(this.lastStatus !== 1){
+        //this.sprite.animations.stop();
+        this.sprite.frame = 10;
+        this.lastStatus = 1;
+      }
+    break;
+    case 2:
+      if(this.lastStatus !== 2){
+        //this.sprite.animations.stop();
+        this.sprite.animations.play('left');
+        this.lastStatus = 2;
+      }
+    break;
+    case 3:
+      if(this.lastStatus !== 3){
+        //this.sprite.animations.stop();
+        this.sprite.animations.play('right');
+        this.lastStatus = 3;
+      }
+    break;
+    case 4:
+      if(this.lastStatus !== 4){
+        //this.sprite.animations.stop();
+        this.lastStatus = 4;
+      }
+    break;
+    case 5:
+      if(this.lastStatus !== 5){
+        //this.sprite.animations.stop();
+        this.lastStatus = 5;
+      }
+    break;
+    case 6:
+      if(this.lastStatus !== 6){
+        //this.sprite.animations.stop();
+        this.sprite.animations.play('death');
+        this.lastStatus = 6;
+      }
+    break;
+    case 100:
+      if (this.lastStatus !== 100) {
+        this.sprite.loadTexture('explorer', 0);
+      }
+    break;
+    case 101:
+      if (this.lastStatus !== 101) {
+        this.sprite.loadTexture('monk', 0);
+      }
+    break;
+    case 102:
+      if (this.lastStatus !== 102) {
+        this.sprite.loadTexture('tron', 0);
+      }
+    break;
+    case 103:
+      if (this.lastStatus !== 103) {
+        this.sprite.loadTexture('wizard', 0);
+      }
+    break;
+    case 104:
+      if (this.lastStatus !== 104) {
+        this.sprite.loadTexture('native', 0);
+      }
+    break;
+    case 105:
+      if (this.lastStatus !== 105) {
+        this.sprite.loadTexture('demon', 0);
+      }
+    break;
+	  }
+  }
 };
 
 module.exports = Survivor;
