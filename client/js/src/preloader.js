a23fdab050eaef1db4f3e35aeb039d939ec6cb2b
Jannis Reinsch <jannis@brntns.com>
2015-07-25 20:52:41 +0200

    fixed animation

--- client/js/src/preloader.js
+++ client/js/src/preloader.js
@@ -12,7 +12,7 @@ Preloader.prototype = {
     this.game.load.image('tiles-1', 'assets/tiles-1.png');
     this.game.load.image('item', 'assets/item.png');
     this.game.load.image('hello', 'assets/hello.png');
-    this.game.load.spritesheet('arrow', 'assets/arrow.png',29,29);
+    this.game.load.spritesheet('arrow', 'assets/arrow.png',32,32);
 
     this.game.load.image('rope_ladder_top_left', 'assets/rope_ladder/ladder_1.png');
     this.game.load.image('rope_ladder_top', 'assets/rope_ladder/ladder_2.png');
