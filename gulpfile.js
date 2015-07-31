4d878b381ae232ba4049d18a71687029b46febc2
Jannis Reinsch <jannis@brntns.com>
2015-07-31 12:54:37 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,6 @@ var gulp = require('gulp')
   , run = require('gulp-run')
   , jshint = require('gulp-jshint')
   , connect = require('gulp-connect')
-  , asar = require('gulp-asar')
   , clean = require('gulp-clean')
   , paths;
 
@@ -28,10 +27,6 @@ paths = {
   js:     ['client/js/src/**/*.js', '!client/js/lib/**/*.js'],
   dist:   ['./dist/']
 };
-
-gulp.task('run', function () {
-  return run('electron . ').exec();
-});
 
 // add custom browserify options here
 var customOpts = {
